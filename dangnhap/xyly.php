086da80298cbfbacfbfc453df4b023778ecfbd42
Trần Thị Hà Giang C2200003 <129876389+GiangHa2001@users.noreply.github.com>
2024-04-14 22:44:35 +0700

    Add files via upload

--- dangnhap/xyly.php
+++ dangnhap/xyly.php
@@ -6,8 +6,6 @@ header('Content-Type: text/html; charset=UTF-8');
 //Xử lý đăng nhập
 if (isset($_POST['dangnhap']))
 {
-//Kết nối tới database
-include('connect.php');
   
 //Lấy dữ liệu nhập vào
 $username = addslashes($_POST['username']);
@@ -21,25 +19,30 @@ exit;
   
 // mã hóa pasword
 // $password = md5($password);
-  
+$connect = mysqli_connect ('localhost', 'root', '', 'webcoffee');
+mysqli_set_charset($connect, 'UTF8');
 //Kiểm tra tên đăng nhập có tồn tại không
-$query = "SELECT * FROM member WHERE username='$username'";
-$result = mysqli_query($connect, $query) or die( mysqli_error($connect));
+$query = mysqli_query($connect, "SELECT * FROM member");
+// $result = mysqli_query($connect, $query) or die( mysqli_error($connect));
 
 // if (!$result) {
 // echo "Tên đăng nhập hoặc mật khẩu không đúng!";
 // } else {
 // echo "Đăng nhập thành công!";
 // }
-  
+  $kt=1;
 //Lấy mật khẩu trong database ra
-$row = mysqli_fetch_array($result);
+while($row = mysqli_fetch_array($query)){
+    if ($password == $row['password']&&$username==$row['username']) { 
+    $kt=0;
+    }
+}
   
 //So sánh 2 mật khẩu có trùng khớp hay không
-if ($password != $row['password']) {
-echo " $password Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
-exit;
-}
+if($kt==1){
+echo "Tài khoảng hoặc mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+exit;}
+
   
 //Lưu tên đăng nhập
 $_SESSION['username'] = $username;
